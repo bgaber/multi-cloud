7991e6d16987eaf556ca9ee4755ff3ff0968c8da
brian.gaber <brian.gaber@primus.ca>
2021-01-26 19:27:48 -0500

    Modified Lambda functions so they work with API Gateway encapsulation

--- aws/lambda/presigned_post.py
+++ aws/lambda/presigned_post.py
@@ -1,31 +1,21 @@
 import json
 import boto3
-#from botocore.exceptions import ClientError
 
-#bucket='bg-us-east-1-bucket'
-#key='filename.jpg'
 s3presigndict = {
   "Content-Type": "image/jpeg"
 }
 expiry=3600
 
 def lambda_handler(event, context):
-    """
-    Method to get post url from s3
-    :param bucket: name of s3 bucket
-    :param key: key of the file
-    :return:
-    """
-    
-    #print("Received event: " + json.dumps(event, indent=2))
-    #print("s3_bucket_value = " + event['bucket'])
-    #print("s3_key_value = " + event['key'])
-    #return event['bucket']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    # Extract parameters this way because API Gateway encapsulates them (https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html)
+    body = json.loads(event['body'])
+    bucket = body['bucket']
+    key = body['key']
+
     try:
         presigned_post = boto3.client('s3').generate_presigned_post(
-            event['bucket'],
-            event['key'],
+            bucket,
+            key,
             Fields=s3presigndict,
             ExpiresIn=expiry
         )
--- aws/lambda/presigned_url.py
+++ aws/lambda/presigned_url.py
@@ -1,30 +1,20 @@
 import json
 import boto3
-#from botocore.exceptions import ClientError
 
-#bucket='bg-us-east-1-bucket'
-#key='filename.jpg'
 expiry=3600
 
 def lambda_handler(event, context):
-    """
-    Method to get url from s3
-    :param bucket: name of s3 bucket
-    :param key: key of the file
-    :return:
-    """
-    
-    #print("Received event: " + json.dumps(event, indent=2))
-    #print("s3_bucket_value = " + event['bucket'])
-    #print("s3_key_value = " + event['key'])
-    #return event['bucket']  # Echo back the first key value
-    #raise Exception('Something went wrong')
+    # Extract parameters this way because API Gateway encapsulates them (https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html)
+    body = json.loads(event['body'])
+    bucket = body['bucket']
+    key = body['key']
+
     try:
         presigned_url = boto3.client('s3').generate_presigned_url(
             ClientMethod='put_object',
             Params={
-                'Bucket': event['bucket'],
-                'Key': event['key'],
+                'Bucket': bucket,
+                'Key': key,
             },
             ExpiresIn=expiry
         )
