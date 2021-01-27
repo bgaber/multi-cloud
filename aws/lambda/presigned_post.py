88db5e91521ecadfe70bab9fc4c0fbbdb4de38a2
brian.gaber <brian.gaber@primus.ca>
2021-01-26 21:57:29 -0500

    modified to handle API GW and Lambda console test

--- aws/lambda/presigned_post.py
+++ aws/lambda/presigned_post.py
@@ -1,16 +1,22 @@
 import json
 import boto3
 
+mode = "apigw" # mode can be apigw or test
+
 s3presigndict = {
   "Content-Type": "image/jpeg"
 }
 expiry=3600
 
 def lambda_handler(event, context):
-    # Extract parameters this way because API Gateway encapsulates them (https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html)
-    body = json.loads(event['body'])
-    bucket = body['bucket']
-    key = body['key']
+    if (mode == "test"):
+        bucket = event['bucket']
+        key = event['key']
+    else:
+        # Extract parameters this way because API Gateway encapsulates them (https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html)
+        body = json.loads(event['body'])
+        bucket = body['bucket']
+        key = body['key']
 
     try:
         presigned_post = boto3.client('s3').generate_presigned_post(
--- aws/lambda/presigned_url.py
+++ aws/lambda/presigned_url.py
@@ -1,13 +1,19 @@
 import json
 import boto3
 
+mode = "apigw"  # mode can be apigw or test
+
 expiry=3600
 
 def lambda_handler(event, context):
-    # Extract parameters this way because API Gateway encapsulates them (https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html)
-    body = json.loads(event['body'])
-    bucket = body['bucket']
-    key = body['key']
+    if (mode == "test"):
+        bucket = event['bucket']
+        key = event['key']
+    else:
+        # Extract parameters this way because API Gateway encapsulates them (https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html)
+        body = json.loads(event['body'])
+        bucket = body['bucket']
+        key = body['key']
 
     try:
         presigned_url = boto3.client('s3').generate_presigned_url(
