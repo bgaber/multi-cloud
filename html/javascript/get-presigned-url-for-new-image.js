b47aac47985481c6eaffd77b93b77c1f97574812
brian.gaber <brian.gaber@primus.ca>
2021-02-04 15:06:15 -0500

    snap pictures

---
+++ html/javascript/get-presigned-url-for-new-image.js
@@ -0,0 +1,51 @@
+function jq_ui_alert(div_id, theMESSAGE) {
+    var theICON = '<span class="ui-icon ui-icon-alert" style="float:left; margin:0 7px 0 0;"></span>';
+    $("#" + div_id).html('<P>' + theICON + theMESSAGE + '</P>');
+    $("#" + div_id).dialog({
+        modal: true,
+        buttons: [{
+            text: "OK",
+            icon: "ui-icon-check",
+            click: function () {
+                $(this).dialog("close");
+            }
+        }]
+    });
+}
+
+function getPreSignedURL(imBlob) {
+    const apigw_endpt = "https://mk4tkjgt8k.execute-api.ca-central-1.amazonaws.com/api";
+
+    var nameOfFile = `${parseInt(Math.random() * 10000000)}.png`;
+
+    if (nameOfFile.length > 0) {
+        // Output computed file name to webpage
+        $("#name_of_file").html('<b>Name of New Image File:</b> ' + nameOfFile);
+        $.ajax({
+            // The URL for the request
+            url: apigw_endpt + "/generate_presigned_url",
+            // The data to send (will be converted to a query string)
+            data: {
+                file_name: nameOfFile, sid: Math.random()
+            },
+            type: "GET",
+            // The type of data expected back
+            dataType: "json",
+            // Code to run if the request succeeds;
+            // the response is passed to the function
+            success: function (json) {
+                handleData(json, imBlob);
+            },
+            // Code to run if the request fails; the raw request and
+            // status codes are passed to the function
+            error: function (xhr, status, errorThrown) {
+                jq_ui_alert('dialog-message', "There was an AJAX problem with the getPreSignedURL function.");
+                console.log("Error: " + errorThrown);
+                console.log("Status: " + status);
+                console.dir(xhr);
+            }
+        });
+    } else {
+        jq_ui_alert('dialog-message', "Invalid name for the new image file.");
+    }
+}
---
+++ html/javascript/handle-existing-image.js
@@ -0,0 +1,29 @@
+function handleData(json, form_data) {
+    // Output S3 Pre-Signed URL to webpage
+    // $("#psurl_result").html('<b>PreSigned URL:</b> ' + json.fields);
+    console.log("Status Return: " + json.status);
+    console.log("PreSign Return: " + json.fields);
+    $.ajax({
+        headers: { 'x-amz-acl': 'public-read' },
+        url: json.fields,
+        type: 'PUT',
+        contentType: 'image/jpeg',
+        data: form_data,
+        processData: false,
+        success: function (data) {
+            jq_ui_alert('dialog-message', "Successful upload to S3 using pre-signed url.");
+            // Output success message to webpage
+            //$("#upload_result").html('<b>Upload Result:</b> Success');
+            console.log("Upload Result: Success");
+        },
+        error: function (xhr, status, errorThrown) {
+            jq_ui_alert('dialog-message', "Unsuccessful upload to S3 using pre-signed url.");
+            // Output failure message to webpage
+            //$("#upload_result").html('<b>Upload Result:</b> Failed');
+            console.log("Upload Result: Failed");
+            console.log("Error: " + errorThrown);
+            console.log("Status: " + status);
+            console.dir(xhr);
+        }
+    });
+}
